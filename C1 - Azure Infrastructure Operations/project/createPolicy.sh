5b5dc94707f58755347a962505fde8101a2acbca
Anthony Ho <anthony.ho.wc@gmail.com>
2021-03-01 18:18:00 +0800

    Match assignment name is the project requirement.

--- C1 - Azure Infrastructure Operations/project/createPolicy.sh
+++ C1 - Azure Infrastructure Operations/project/createPolicy.sh
@@ -2,4 +2,4 @@
 # The command to creat ethe policy of at least one tag from CLI + JSON definition
 
 az policy definition create --name AtLeastOneTagPolicyDefinition --display-name "At Least One Tag Policy" --description "There must be at least one tag associated to all resource created." --mode Indexed --rules policy_rules.json
-az policy assignment create --name AtLeastOneTagPolicyAssignment --display-name "At Least One Tag Policy Assignment" --policy AtLeastOneTagPolicyDefinition
+az policy assignment create --name tagging-policy --display-name "At Least One Tag Policy Assignment" --policy AtLeastOneTagPolicyDefinition
